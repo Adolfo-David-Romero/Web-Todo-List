84375b9977eae3ec731057e7d6616fd11d925ac4
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2025-04-22 22:32:25 -0400

    Toggle button works and deletion

--- script.js
+++ script.js
@@ -9,9 +9,19 @@ function addTask(){
         let li = document.createElement("li"); //create html element with tag name li
         li.innerHTML = inputBox.value; //add text to li (input box)
         listContainer.appendChild(li); //add li element to "list container"
-        let span = document.createElement("span");
-        span.innerHTML="\u00d7";
-        li.appendChild(span);
+
+        let span = document.createElement("span"); //create span
+        span.innerHTML="\u00d7"; //in the span, create a cross-icon
+        li.appendChild(span); //display cross icon 
         }
     inputBox.value=""//reset box
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){ //check if li is licked
+        e.target.classList.toggle("checked"); //add check class name
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove(); //delete li element
+    }
+}, false);
